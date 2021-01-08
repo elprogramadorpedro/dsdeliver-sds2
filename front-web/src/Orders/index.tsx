c0097e5cb440a17fc5bc3e01614759d2e8a128b8
Pedro <elprogramadorpedro@gmail.com>
2021-01-08 15:51:39 -0300

    Add ProductCard, types, ProductsList

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react';
+import ProductsList from './ProductsList';
 import StepeHeader from './StepHeader';
+import {Product} from './types'
+import {fetchProducts} from '../api'
 import './styles.css';
 
 
 function Orders() {
+ const [products, setProducts]= useState<Product []>([]);
+
+    useEffect(()=>{
+        
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+    }, []);
+
     return (
         <div className="orders-container">    
            <StepeHeader/>
+           <ProductsList products={products}/>
         </div>
     )
 }
